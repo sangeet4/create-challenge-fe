b3753fe34535420e17456cde63db908211a66867
sangeet4 <and.sangeet@gmail.com>
2018-11-02 09:42:18 +0530

    AM not triggering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { DescriptionComponent } from './description/description.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule,
+        MatSelectModule,
+        MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/description/description.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-description',
+  templateUrl: './description.component.html',
+  styleUrls: ['./description.component.css']
+})
+export class DescriptionComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) {}
+
+  availbLang = [ 'Java' ];
+
+  questDesc: FormGroup;
+
+  ngOnInit() {
+    this.questDesc = this.fb.group({
+      challengeTitle : ['',
+      [Validators.required]],
+      challengeDesc : ['',
+      [Validators.required]],
+      problemStat : ['',
+      [Validators.required]],
+      inputFormat : ['',
+      [Validators.required]],
+      constraints : ['',
+      [Validators.required]],
+      outputFormat : ['',
+      [Validators.required]],
+      maxScore : ['',
+      [Validators.required,
+      Validators.min(5),
+      Validators.max(100)]],
+      maxRuntime : ['', Validators.required],
+      progLang : ['', Validators.required],
+      level : ['', Validators.required]
+    });
+  }
+
+  get title() { return this.questDesc.get('challengeTitle'); }
+
+  get desc() { return this.questDesc.get('challengeDesc'); }
+
+  get stat() { return this.questDesc.get('problemStat'); }
+
+  get inFormat() { return this.questDesc.get('inputFormat'); }
+
+
+  onSubmit() {
+    if (this.questDesc.invalid) {
+      return;
+    }
+    console.log(this.questDesc.value);
+    alert('The form was submitted');
+    this.questDesc.reset();
+  }
+
+}
